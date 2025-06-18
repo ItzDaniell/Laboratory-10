04a1ca1dc714e10b191ee3de6b7d150794315e13
Andersonbaltazar <franklin.baltazar@tecsup.edu.pe>
2025-06-18 15:29:52 -0500

    no se si funcione

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,11 +1,21 @@
-function HeaderComponent() {
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
+function HeaderComponent() {
+    const { usuario, logout } = useContext(AppContext);
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
+        navigate('/');
+    };
 
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
                 <span className="navbar-brand mb-0 h1">SeriesApp</span>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarText">
@@ -21,15 +31,15 @@ function HeaderComponent() {
                         </li>
                     </ul>
                     <div>
-                        Bienvenido Juan Daniel 
+                        Bienvenido {Usuario ? Usuario.name : 'Invitado'}
                         <div className="text-end">
-                            <a href="/">Salir</a>
-                        </div> 
+                            <button onClick={handleLogout} className="btn btn-link">Salir</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </nav>
-    )
+    );
 }
-  
-export default HeaderComponent
+
+export default HeaderComponent;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,65 +1,93 @@
+import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AppContext } from "../contexts/AppContext"; 
+import { loginService } from "../services/LoginServices";
 
+const initData = {
+  username: "",
+  password: ""
+};
 
-function LoginPage(){
-    const navigate = useNavigate();
-    
+function LoginPage() {
+  const navigate = useNavigate();
+  const { login } = useContext(AppContext);
+  const [data, setData] = useState(initData);
 
-    function logear(e){
-        e.preventDefault();
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
+  const onChangeUserName = (e) => {
+    const nData = { ...data, username: e.target.value };
+    setData(nData);
+  };
 
-        if (email == "juan@gmail.com" && password == "juan123") {
-            navigate("/home");
-        } else {
-            alert("Credenciales incorrectas. Por favor, verifica tu correo electrónico y contraseña.");
-        }
+  const onChangePassword = (e) => {
+    const nData = { ...data, password: e.target.value };
+    setData(nData);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const resp = await loginService(data);
+      console.log("Respuesta del loginService:", resp.data);
+      login(resp.data);
+      navigate("/series");
+    } catch (error) {
+      console.error("Error en el login:", error);
+      window.alert('El usuario o contraseña no es correcto. Por favor, inténtalo de nuevo.');
+      setData(initData); 
     }
+  };
 
-    return (
-    	<section className="d-flex justify-content-center align-items-center min-vh-100">
-            <div className="container">
-                <div className="row justify-content-sm-center">
-                    <div className="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
-                        <div className="card shadow-lg">
-                            <div className="card-body p-5">
-                                <h1 className="fs-4 card-title fw-bold mb-4">Login</h1>
-                                <form autoComplete="off">
-                                    <div className="mb-3">
-                                        <label className="mb-2 text-muted" htmlFor="email">E-Mail</label>
-                                        <input id="email" type="email" className="form-control" name="email" required autoFocus />
-                                    </div>
-                                    <div className="mb-3">
-                                        <div className="mb-2 w-100">
-                                            <label className="text-muted" htmlFor="password">Contraseña</label>
-                                            <a href="/forgot" className="float-end">
-                                                Recuperar Contraseña?
-                                            </a>
-                                        </div>
-                                        <input id="password" type="password" className="form-control" name="password" required />
-                                    </div>
-                                    <div className="d-flex align-items-center">
-                                        <div className="form-check">
-                                            <input type="checkbox" name="remember" id="remember" className="form-check-input" />
-                                            <label htmlFor="remember" className="form-check-label">Recordarme</label>
-                                        </div>
-                                        <button onClick={logear} className="btn btn-primary ms-auto">
-                                            Ingresar
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                        <div className="text-center mt-5 text-muted">
-                            Copyright &copy; Tecsup 2025
-                        </div>
+  return (
+    <section className="d-flex justify-content-center align-items-center min-vh-100">
+      <div className="container">
+        <div className="row justify-content-sm-center">
+          <div className="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+            <div className="card shadow-lg">
+              <div className="card-body p-5">
+                <h1 className="fs-4 card-title fw-bold mb-4">Login</h1>
+                <form onSubmit={handleSubmit} autoComplete="off">
+                  <div className="mb-3">
+                    <label className="mb-2 text-muted" htmlFor="username">Usuario</label>
+                    <input 
+                      onChange={onChangeUserName} 
+                      id="username" 
+                      type="text" 
+                      className="form-control" 
+                      name="username" 
+                      required 
+                      autoFocus 
+                      value={data.username} 
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <div className="mb-2 w-100">
+                      <label className="text-muted" htmlFor="password">Contraseña</label>
+                      <a href="/forgot" className="float-end">
+                        Recuperar Contraseña?
+                      </a>
                     </div>
-                </div>
+                    <input id="password" type="password" className="form-control" name="password" required value={data.password} onChange={onChangePassword} />
+                  </div>
+                  <div className="d-flex align-items-center">
+                    <div className="form-check">
+                      <input type="checkbox" name="remember" id="remember" className="form-check-input" />
+                      <label htmlFor="remember" className="form-check-label">Recordarme</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary ms-auto">
+                      Ingresar
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
-    	</section>
-    );
+            <div className="text-center mt-5 text-muted">
+              Copyright &copy; Tecsup 2024
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
-
 
 export default LoginPage;
