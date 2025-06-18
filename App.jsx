3be93a53f7bb0c4d5fc5b533b24963fff9a09ca6
Rafael-129 <rafael.garcia.c@tecsup.edu.pe>
2025-06-18 14:58:06 -0500

    cambios

---
+++ App.jsx
@@ -0,0 +1,37 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppProvider } from "./context/AppContext"
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import CategoryPage from "./pages/CategoryPage";
+import CategoryFormPage from "./pages/CategoryFormPage";
+import SeriePage  from "./pages/SeriePage";
+import ForgotPage from "./pages/ForgotPage";
+import CategoryEditFormPage from "./pages/CategoryEditFormPage";
+import SerieFormPage from "./pages/SerieFormPage";
+import SerieEditFormPage from './pages/SerieEditFormPage';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+        <AppProvider>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/forgot" element={<ForgotPage />} />
+          <Route path="/categories" element={<CategoryPage />} />
+          <Route path='/categories/new' element={<CategoryFormPage />} />
+          <Route path='/categories/edit/:id' element={<CategoryEditFormPage />} />
+
+          <Route path="/series" element={<SeriePage />} />
+          <Route path="/series/new" element={<SerieFormPage />} />
+          <Route path="/series/edit/:id" element={<SerieEditFormPage />} />
+
+        </Routes>
+      </AppProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
